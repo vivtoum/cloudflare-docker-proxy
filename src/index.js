ee4d4ef65cf17c27b3f19dbba3e8ff2708ab80cd
VivToum <vedyou@qq.com>
2024-06-12 09:40:01 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.vivtoum.work": "https://registry-1.docker.io",
+  "quay.vivtoum.work": "https://quay.io",
+  "gcr.vivtoum.work": "https://gcr.io",
+  "k8s-gcr.vivtoum.work": "https://k8s.gcr.io",
+  "k8s.vivtoum.work": "https://registry.k8s.io",
+  "ghcr.vivtoum.work": "https://ghcr.io",
+  "cloudsmith.vivtoum.work": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
